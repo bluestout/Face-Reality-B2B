d038cb301a59a448e1534e57346d92c05d6ed061
Matjaz Zorko <matjaz@apto.si>
2018-12-03 11:13:04 +0100

    Certified video slicing, implementation, responsive

--- src/assets/scripts/layout/theme.js
+++ src/assets/scripts/layout/theme.js
@@ -18,7 +18,7 @@ import "../custom/faq";
 import "../custom/information-center";
 import "../custom/header";
 import "../custom/image-compare";
-import "../custom/home-banner";
+import "../custom/home-banner-slick";
 import "../custom/register";
 import "../custom/account";
 import "../custom/qty-changer";
