2d1662245f681b1d6705c06993da1f057e129d0d
Matjaz Zorko <matjaz@apto.si>
2018-12-12 17:50:29 +0100

    Assets page - forced download script

--- src/assets/scripts/custom/pages/assets.js
+++ src/assets/scripts/custom/pages/assets.js
@@ -4,6 +4,7 @@ const el = {
   search: "[data-assets-quick-search]",
   item: "[data-asset-item]",
   title: "[data-asset-item-title]",
+  downlaod: "[data-download-asset]",
 };
 
 function quickSearch() {
@@ -24,4 +25,30 @@ function quickSearch() {
   }, 200);
 }
 
+function forceDownload(link) {
+  const url = link.attr("href");
+  const fileName = link.attr("download");
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "blob";
+  xhr.onload = function() {
+    const urlCreator = window.URL || window.webkitURL;
+    const imageUrl = urlCreator.createObjectURL(this.response);
+    const tag = document.createElement("a");
+    tag.href = imageUrl;
+    tag.download = fileName;
+    document.body.appendChild(tag);
+    tag.click();
+    document.body.removeChild(tag);
+  };
+  xhr.send();
+}
+
+function downloadInit(event) {
+  event.preventDefault();
+  forceDownload($(event.currentTarget));
+}
+
 $(document).on("change, keydown", el.search, quickSearch);
+
+$(document).on("click", el.downlaod, downloadInit);
