6aeb03a0efc6e1e3cb274c5da0a669dd052fa9c7
Matjaz Zorko <matjaz@apto.si>
2019-01-30 17:54:47 +0100

    Replace sticky sidebar with library

--- src/assets/scripts/custom/components/sticky-sidebar.js
+++ src/assets/scripts/custom/components/sticky-sidebar.js
@@ -1,91 +1,23 @@
 import $ from "jquery";
 
 const el = {
-  container: "[data-sticky-container]",
-  sidebar: "[data-sticky-sidebar]",
+  sticky: ".js-sticky-sidebar",
+  container: ".js-sticky-container",
   header: "[data-section-id='header']",
 };
 
-let lastKnownScrollPos = 0;
-let ticking = false;
-
-window.addEventListener("scroll", () => {
-  lastKnownScrollPos = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(() => {
-      stickySidebar(lastKnownScrollPos);
-      ticking = false;
+function stickyInit() {
+  if ($(".js-sticky-sidebar").length > 0 && $(window).width() > 991) {
+    const filter = $("body").hasClass("template-collection") ? 60 : 0;
+    const headerHeight = $(el.header).outerHeight() + filter + 20;
+    const sidebar = new StickySidebar(".js-sticky-sidebar", {
+      topSpacing: headerHeight,
+      bottomSpacing: 10,
+      containerSelector: ".js-sticky-sidebar-container",
     });
-
-    ticking = true;
   }
-});
-
-function stickySidebar(lastPosition, padding = 90) {
-  $(el.sidebar).each(function() {
-    const $this = $(this);
-    if ($(window).width() > 991) {
-      const $container = $this.closest(el.container);
-      const headerH = $(el.header).outerHeight() || 0;
-      const windowH = $(window).height();
-      if ($container.length > 0) {
-        const containerOffset = $container.offset().top;
-        const containerHeight = $container.height();
-        const maxH = containerHeight - containerOffset - headerH - padding;
-        const sidebarH = $this.height();
-
-        if (lastPosition > containerOffset - headerH) {
-          if (lastPosition > maxH) {
-            $this.css({ bottom: 0, top: "" });
-          } else {
-            $this.css({
-              bottom:
-                containerHeight -
-                sidebarH -
-                lastPosition +
-                containerOffset -
-                headerH -
-                padding,
-              top: "",
-            });
-          }
-        } else {
-          $this.css({ top: 0, bottom: null });
-        }
-      }
-    } else {
-      $this.css({ bottom: "", top: "", "max-height": "100%" });
-    }
-  });
 }
 
-function prepSticky() {
-  $(el.sidebar).each(function() {
-    const $this = $(this);
-    $this.closest(el.container).css("min-height", $this.height());
-    if ($(window).width() > 991) {
-      $this.addClass("sticky-active").css({
-        height: $this.height(),
-      });
-    } else {
-      $this.removeClass("sticky-active").css({
-        height: "auto",
-      });
-    }
-  });
-}
-
-$(document).ajaxComplete(stickySidebar);
 $(document).ready(() => {
-  document.addEventListener("runFilter", () => {
-    lastKnownScrollPos = window.scrollY;
-    stickySidebar(lastKnownScrollPos);
-    setTimeout(() => {
-      lastKnownScrollPos = window.scrollY;
-      stickySidebar(lastKnownScrollPos);
-    }, 201);
-  });
+  stickyInit();
 });
-
-$(window).on("load resize", prepSticky);
